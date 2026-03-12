789c402cd77ea7dbc443d51c6ad9bc9f94970d5d
Khusan Abdukodirov <khusan@gmail.com>
2026-03-12 21:58:49 +0900

    feat: deploy shell integratiion with docker compose

--- src/libs/config.ts
+++ src/libs/config.ts
@@ -1,5 +1,5 @@
 import { ObjectId } from 'bson';
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 import * as path from 'path';
 import { T } from './types/common';
 
@@ -34,7 +34,7 @@ export const availableCommentSorts = ['createdAt', 'updatedAt'];
 export const validMimeTypes = ['image/png', 'image/jpg', 'image/jpeg'];
 export const getSerialForImage = (filename: string) => {
 	const ext = path.parse(filename).ext;
-	return uuidv4() + ext;
+	return randomUUID() + ext;
 };
 
 export const shapeIntoMongoObjectId = (target: any) => {
